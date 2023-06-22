34bd54f0fc67e1125916800c08e47442f262eecc
hiro1117 <hasihiro1117@gmail.com>
2023-06-22 14:25:54 +0900

    little update

--- assignment08/script.js
+++ assignment08/script.js
@@ -7,12 +7,18 @@
 
   const getAction5Label = async () => {
     const resp = await kintone.api(`/k/v1/app/form/fields.json`, 'GET', params);
-    const labelArray = Object.keys(resp.properties.Table.fields.Action5.options);
-    return labelArray;
-  }
+    const options = resp.properties.Table.fields.Action5.options;
+    const orderedOptionLabel = [];
 
-  kintone.events.on('app.record.create.show', async (event) => {
-    const action5Array = await getAction5Label();
+    for (const element of Object.keys(options)) {
+      const index = options[element].index;
+      orderedOptionLabel[index] = element;
+    };
+
+    return orderedOptionLabel;
+  };
+
+  const insertTable = async(event, action5Array) => {
     const tableArray = event.record.Table.value;
     action5Array.forEach((elem) => {
       let clonedTableArrayElem = structuredClone(tableArray[0]);
@@ -20,6 +26,13 @@
       tableArray.push(clonedTableArrayElem);
     });
     tableArray.shift();
+  };
+
+
+
+  kintone.events.on('app.record.create.show', async (event) => {
+    const action5Array = await getAction5Label();
+    insertTable(event, action5Array);
     return event;
-  }); 
+  });
 })();
