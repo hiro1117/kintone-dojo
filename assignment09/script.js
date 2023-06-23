e1e4dbb04e3484794130f57788a7f693e7c498bc
hiro1117 <hasihiro1117@gmail.com>
2023-06-23 15:47:18 +0900

    little update

--- assignment09/script.js
+++ assignment09/script.js
@@ -1,19 +1,10 @@
 (() => {
   'use strict';
 
-  const submitEvents = [
-    'app.record.create.submit', 
-    'app.record.edit.submit',
-  ];
-
-  const params = {
-    app: kintone.api.getId(),
-    fields: '重複禁止項目',
-  };
+  
 
   // REST APIでレコードの重複禁止項目を配列で取得
   const getRestrictedStrs = async () => {
-    const resp = await kintone.api(kintone.api.url(`/k/v1/records.json`, true), 'GET', params);
     const recordsArray = resp.records;
     const recordValuesArray = [];
     
@@ -24,20 +15,23 @@
     return recordValuesArray;
   };
   
-  // 生成された重複禁止項目とレコードの重複禁止項目を比較
-  const compareUniqueStrWithRecords = (recordArray) => {
-
-  };
   
   // 指定したイベントによって、重複禁止処理を実行
-  kintone.events.on(submitEvents, async (event) => {
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], async (event) => {
+    const fieldsRestrictedId =  event.record.重複禁止項目.value;
+    console.log('query');
     
+    const params = {
+      app: kintone.api.getId(),
+      fields: '重複禁止項目',
+      query: `重複禁止項目 = ${fieldsRestrictedId}`
+    };
+
+    console.log(params.query);
+    const resp = await kintone.api(kintone.api.url(`/k/v1/records.json`, true), 'GET', params);
+
+    console.log(fieldsRestrictedId);
     const restrictedValuesArray = await getRestrictedStrs();
-    console.log(restrictedValuesArray);
-    const isuser = window.confirm('g')
-    if (isuser) console.log('gg')
-    else console.log('ggg')
-    const isUniqueStrExist = compareUniqueStrWithRecords(restrictedValuesArray)
 
     return event;
   })
