6a9e93e6eed897c4d9011a5d5a4b876aa2f2d9c4
hiro1117 <hasihiro1117@gmail.com>
2023-06-23 17:53:19 +0900

    update assignment09

--- assignment09/script.js
+++ assignment09/script.js
@@ -6,12 +6,10 @@
     'app.record.edit.submit',
   ];
   
-  
   kintone.events.on(submitEvents, async (event) => {
     // JS APIで重複禁止項目の値を取得
     const recordValue = event.record.重複禁止項目.value;
-    
-    
+     
     // 重複禁止項目の値を含めたparamを用意
     const params = {
       "app": kintone.app.getId(),
@@ -19,19 +17,10 @@
       "totalCount": true
     };
 
-  // REST APIをparamを使って叩く（param = '重複項目 = "20230612-KN-1" ）
+    // REST APIをparamを使って叩く（param = '重複項目 = "20230612-KN-1" ）
     const resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params);
-    console.log(resp);
-
-  // if 返ってくるRecord.lengthが０、return event 
-  // else window.confirm("dddddd");
+    
     if (Number(resp.totalCount) === 0) return event;
     else return window.confirm('レコードが重複しています。このまま保存しますか？') ? event : false;
-
   })
-
-
-
-
-
 })();
