c85dec2756f0f852fd6a91b04902416e5bec01f3
hiro1117 <hasihiro1117@gmail.com>
2023-06-29 12:53:04 +0900

    fix script.js

--- assignment09/script.js
+++ assignment09/script.js
@@ -2,25 +2,25 @@
   'use strict';
 
   const submitEvents = [
-    'app.record.create.submit', 
+    'app.record.create.submit',
     'app.record.edit.submit',
   ];
-  
+
   kintone.events.on(submitEvents, async (event) => {
     // JS APIで重複禁止項目の値を取得
+    const RECORD_ID = event.recordId;
     const recordValue = event.record.重複禁止項目.value;
-     
+    const query = RECORD_ID ? `重複禁止項目 = "${recordValue}" and レコード番号 != "${RECORD_ID}"` : `重複禁止項目 = "${recordValue}"`;
     // 重複禁止項目の値を含めたparamを用意
     const params = {
-      "app": kintone.app.getId(),
-      "query": `重複禁止項目 = "${recordValue}"`,
-      "totalCount": true
+      app: kintone.app.getId(),
+      query: query,
+      totalCount: true
     };
 
     // REST APIをparamを使って叩く（param = '重複項目 = "20230612-KN-1" ）
     const resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params);
-    
     if (Number(resp.totalCount) === 0) return event;
-    else return window.confirm('レコードが重複しています。このまま保存しますか？') ? event : false;
+    return window.confirm('レコードが重複しています。このまま保存しますか？') ? event : false;
   });
 })();
